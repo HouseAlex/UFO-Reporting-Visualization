1e1832358aaaa34695891eee9233ec97c7c6ba3e
Ted Cordonnier <tedcordonnier@gmail.com>
2024-03-31 18:41:19 -0400

    Added histogram and made changes elsewhere as needed

--- js/main.js
+++ js/main.js
@@ -12,7 +12,22 @@ d3.csv('data/ufo_sightings.csv')
         parameter: 'ufo_shape'
     }, sightings);
     pieChart.UpdateVis();
+
+    histogram = new Histogram({
+        parentElement: '#histogram',
+        parameter: 'encounter_length'
+    }, sightings);
+    histogram.UpdateVis();
+
+    histogram2 = new Histogram({
+        parentElement: '#histogram2',
+        parameter: 'encounter_length'
+    }, sightings);
+    histogram2.UpdateVis();
+
     
     pieChartReset.on("click", d => pieChart.ResetArcColors())
 })
 .catch(error => console.error(error));
+
+
